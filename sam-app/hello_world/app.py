3f0e595b35af2d97ace9dae8647641b2fa4c8a71
JUAN GOMEZ <36505610-juanegomez@users.noreply.replit.com>
2025-06-14 13:06:17 +0000

    .

--- sam-app/hello_world/app.py
+++ sam-app/hello_world/app.py
@@ -10,7 +10,7 @@ def lambda_handler(event, context):
         200,
         "body":
         json.dumps({
-            "message": "hello world",
+            "message": "hello class",
             # "location": ip.text.replace("\n", "")
         }),
     }
